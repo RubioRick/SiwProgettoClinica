0b66543d34ca16822100e6049525b37c6e1169f9
huly94 <ico.giulianelli@hotmail.it>
2016-06-10 10:46:23 +0200

    Inserimento Dao RisultatiEsame e Paziente

--- src/it/uniroma3/db/dao/jpa/PazienteDaoJpa.java
+++ src/it/uniroma3/db/dao/jpa/PazienteDaoJpa.java
@@ -1,32 +1,47 @@
 package it.uniroma3.db.dao.jpa;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import it.uniroma3.db.dao.PazienteDao;
 import it.uniroma3.db.models.Paziente;
 
+@Stateless(name = "pazFacade")
 public class PazienteDaoJpa implements PazienteDao {
+	@PersistenceContext(unitName = "clinica")
+    private EntityManager em;
 
 	@Override
 	public void save(Paziente p) {
-		// TODO Auto-generated method stub
+		this.em.persist(p);
 
 	}
 
 	@Override
 	public void remove(Paziente p) {
-		// TODO Auto-generated method stub
+		this.em.remove(p);
 
 	}
 
 	@Override
 	public void update(Paziente p) {
-		// TODO Auto-generated method stub
+		Query query = this.em.createQuery("UPDATE Paziente SET nome = ?1, cognome = ?2, dataNascita = ?3, email = ?4,  WHERE codicefiscale = ?5");
+		query.setParameter(1,p.getNome());
+		query.setParameter(2,p.getCognome());
+		query.setParameter(3,p.getDataDiNascita());
+		query.setParameter(4,p.getEmail());
+		query.setParameter(5,p.getCodiceFiscale());
+		
+	
 
 	}
 
 	@Override
 	public Paziente findByPrimaryKey(String cf) {
-		// TODO Auto-generated method stub
-		return null;
+		Paziente p = this.em.find(Paziente.class, cf);
+		return p;
 	}
 
 }
