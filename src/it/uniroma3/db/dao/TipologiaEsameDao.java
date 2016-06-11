9b5e087d5b16694db1d2eb1ff62fae2ba597c205
huly94 <ico.giulianelli@hotmail.it>
2016-06-11 17:39:23 +0200

    Aggiunta findAll a TipologiaEsameDao e modifica Welcome.jsp

--- src/it/uniroma3/db/dao/TipologiaEsameDao.java
+++ src/it/uniroma3/db/dao/TipologiaEsameDao.java
@@ -1,5 +1,7 @@
 package it.uniroma3.db.dao;
 
+import java.util.List;
+
 import it.uniroma3.db.models.TipologiaEsame;
 
 public interface TipologiaEsameDao {
@@ -7,4 +9,5 @@ public interface TipologiaEsameDao {
 	public void remove(TipologiaEsame t);
 	public void update(TipologiaEsame t);
 	public TipologiaEsame findByPrimaryKey(long id);
+	public List<TipologiaEsame> findAll();
 }
--- src/it/uniroma3/db/dao/jpa/TipologiaEsameDaoJpa.java
+++ src/it/uniroma3/db/dao/jpa/TipologiaEsameDaoJpa.java
@@ -1,12 +1,17 @@
 package it.uniroma3.db.dao.jpa;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
 
 import it.uniroma3.db.dao.TipologiaEsameDao;
 import it.uniroma3.db.models.TipologiaEsame;
+import it.uniroma3.model.Product;
+
 
 @Stateless(name = "tipFacade")
 public class TipologiaEsameDaoJpa implements TipologiaEsameDao {
@@ -36,6 +41,15 @@ public class TipologiaEsameDaoJpa implements TipologiaEsameDao {
 	public TipologiaEsame findByPrimaryKey(long id) {
 		TipologiaEsame t = this.em.find(TipologiaEsame.class, id);
 		return t;
+	}
+
+	
+	@Override
+	public List<TipologiaEsame> findAll() {
+		 CriteriaQuery<TipologiaEsame> cq = em.getCriteriaBuilder().createQuery(TipologiaEsame.class);
+	        cq.select(cq.from(TipologiaEsame.class));
+	        List<TipologiaEsame> tipologieEsame = em.createQuery(cq).getResultList();
+			return tipologieEsame;
 	}
 
 }
